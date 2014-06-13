884db688fc62f4e908ec0fecebaed12eee0125fe
Romain Beuque <romain.beuque@gmail.com>
2014-06-13 11:32:20 +0200

    retrait du controllers groups non existant (RB)

--- Brie/brie/controllers/root.py
+++ Brie/brie/controllers/root.py
@@ -27,7 +27,6 @@ from brie.controllers.error import ErrorController
 from brie.controllers.registration import RegistrationController
 from brie.controllers.treasury import TreasuryController
 from brie.controllers.plugins import PluginsController
-from brie.controllers.groups import GroupsController
 
 
 __all__ = ['RootController']
